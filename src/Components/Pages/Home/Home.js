cd633a4eb64f08a0f21f1673c5988837a77eabc0
mahin-wd <wdmahin.mhm@gmail.com>
2022-12-05 23:15:32 +0600

    added responsive home page

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import lgImg from '../../../images/photo-01-01.png'
+import smImg from '../../../images/sm_photo.png'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            {/* Large devices */}
+            <div className='relative hidden xl:flex'>
+                <img className='max-w-[500px] absolute left-96 xl:top-60 z-10' src={lgImg} alt="" />
+                <div className='bg-[#0E1828] 2xl:w-[1200px] xl:w-[700px] md:w-[500px] absolute right-0 h-screen'>
+                    <div className='pt-96 text-start 2xl:ml-[500px] xl:ml-[300px] lg:ml-[200px]'>
+                        <h3 className='text-xl tracking-widest'>Hi There,</h3>
+                        <h2 className='text-4xl mt-5 mb-1'>This is</h2>
+                        <h1 className='2xl:text-9xl xl:text-8xl font-bold'>Mahin</h1>
+                        <p className='text-gray-300'>I am a front end web developer. I develop the user interface of a website using React Js. </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* small device */}
+            <div className='px-5'>
+                <div className='relative xl:flex-col-reverse 2xl:hidden lg:ml-60'>
+                    <div className='xl:hidden'>
+                        <div className='pt-10 text-center w-3/4 mx-auto'>
+                            <h3 className='text-xl tracking-widest'>Hi There,</h3>
+                            <h2 className='text-4xl mt-5 mb-1'>This is</h2>
+                            <h1 className='2xl:text-9xl xl:text-8xl font-bold'>Mahin</h1>
+                            <p className='text-gray-300'>I am a front end web developer. I develop the user interface of a website using React Js. </p>
+                        </div>
+                        <img className='max-w-[500px] w-[250px] lg:w-[400px] xl:top-60 z-10 xl:hidden mx-auto pt-10' src={smImg} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className='relative'>
-            <div className='absolute top-5 right-5 text-5xl text-white lg:hidden'>
+        <div className='relative z-10'>
+            <div className='absolute top-5 right-5 text-5xl text-white lg:hidden z-20'>
                 {
                     open ? <HiX onClick={() => setOpen(!open)} /> : <HiMenuAlt3 onClick={() => setOpen(!open)} />
                 }
             </div>
-            <div className={`flex flex-col absolute pt-60 bg-[#0E1828] h-screen nav-container lg:left-0 ${open ? 'left-0' : '-left-96'}`}>
+            <div className={`flex flex-col absolute pt-40 bg-[#0E1828] h-screen nav-container lg:left-0 ${open ? 'left-0' : '-left-96'}`}>
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/">Home</Link>
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/skills">Skills</Link>
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/projects">Projects</Link>
