07eae1fa39d3ef6e6db77b64a02fdaad5d929dbd
mahin-wd <wdmahin.mhm@gmail.com>
2022-12-06 00:22:55 +0600

    added social links

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 import { HiMenuAlt3, HiX } from "react-icons/hi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
@@ -19,7 +20,17 @@ const Navbar = () => {
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/projects">Projects</Link>
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/about">About</Link>
                 <Link className='text-4xl font-semibold text-start pl-20 w-80 py-5 text-white' to="/contact">Hire Me</Link>
+
+                <div className=' flex justify-center absolute bottom-5 left-0 right-0'>
+                    <a target="_blank" href="https://github.com/mahin-wd">
+                        <FaGithub className='mx-5 text-3xl' />
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/wd-mahin/">
+                        <FaLinkedin className='mx-5 text-3xl' />
+                    </a>
+                </div>
             </div>
+
         </div>
     );
 };
