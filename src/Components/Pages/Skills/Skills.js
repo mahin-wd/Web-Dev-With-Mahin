91e3605ca1aab513a9053479b1e0668c3435dbc0
mahin-wd <wdmahin.mhm@gmail.com>
2022-12-06 19:11:20 +0600

    added skills

--- src/Components/Pages/Skills/Skills.js
+++ src/Components/Pages/Skills/Skills.js
@@ -1,9 +1,106 @@
 import React from 'react';
+import './Skills.css'
 
 const Skills = () => {
     return (
-        <div className='pt-80'>
-            <h3 className='text-4xl'>Skills page is under development...</h3>
+        <div className='h-screen pt-20'>
+            <h3 className="text-5xl text-[#5bc5f4] font-semibold">My Skills</h3>
+
+            {/* HTML */}
+            <div>
+                <div className='flex justify-center items-center pt-[150px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>90%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-80'>HTML</p>
+                </div>
+            </div>
+
+            {/* CSS */}
+            <div className='-mt-20'>
+                <div className='flex justify-center items-center pt-[120px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>90%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-80'>CSS</p>
+                </div>
+            </div>
+
+            {/* javascript */}
+            <div className='-mt-20'>
+                <div className='flex justify-center items-center pt-[120px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress-js">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>80%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-[270px]'>JavaScript</p>
+                </div>
+            </div>
+
+            {/* react js */}
+            <div className='-mt-20'>
+                <div className='flex justify-center items-center pt-[120px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress-react">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>85%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-[270px]'>React Js</p>
+                </div>
+            </div>
+
+            {/* express js */}
+            <div className='-mt-20'>
+                <div className='flex justify-center items-center pt-[120px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress-express-mongo">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>85%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-[270px]'>Express Js</p>
+                </div>
+            </div>
+
+            {/* mongodb */}
+            <div className='-mt-20'>
+                <div className='flex justify-center items-center pt-[120px] px-10'>
+                    <div className="progress-container">
+                        <div className="progress-express-mongo">
+                        </div>
+                    </div>
+                    <div className='ml-10'>
+                        <p>85%</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='-ml-[270px]'>MongoDB</p>
+                </div>
+            </div>
         </div>
     );
 };
